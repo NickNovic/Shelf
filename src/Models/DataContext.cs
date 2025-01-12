c53920453ed679dfa7f3ad6cd0a06fc629a0dfb8
NickNovic <thenicknovicov@gmail.com>
2025-01-12 13:28:40 +0200

    DataContext now inherits from IDisposable

--- src/Models/DataContext.cs
+++ src/Models/DataContext.cs
@@ -5,20 +5,29 @@
 // connect to the database and execute queries.
 // ============================================================
 
-using src.Models.Abstractions;
-
 namespace src.Models;
 
-public abstract class DataContext
+public abstract class DataContext : IDisposable
 {
     public DataContext()
     {
         Setup();
     }
+
+    public virtual void Dispose()
+    {
+        Disconnect();
+    }
+
     /// <summary>
     /// This method is called when DataContext is created.
     /// It's used to set up DataContext.
     /// For example, it can be used to set up database connection.
     /// </summary>
     public abstract void Setup();
+
+    public virtual void Disconnect()
+    {
+        Console.WriteLine("Disconnect");
+    }
 }
