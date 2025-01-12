c6614885894a8fe1b829d46fee13aa398698d852
NickNovic <thenicknovicov@gmail.com>
2025-01-12 10:55:59 +0200

    Dataset created, but there is nothing implemented

---
+++ src/Models/Dataset.cs
@@ -0,0 +1,96 @@
+// ============================================================
+// File Name: Dataset.cs
+// Created On: 10-01-2025
+// Description: Default implementation of IDataset<T>
+// ============================================================
+
+using System.Linq.Expressions;
+using System.Collections;
+using src.Models.Abstractions;
+
+namespace src.Models;
+
+public class Dataset<T> : IDataset<T>, IQueryable<T>, IQueryable, IEnumerable<T>, IEnumerable where T : class
+{
+    public Expression Expression { get; }
+    
+    public IQueryProvider Provider { get; }
+    
+    public Dataset(IQueryProvider provider, Expression expression)
+    {
+        Provider = provider; 
+        Expression = expression;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return Provider.Execute<IEnumerable<T>>(Expression).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddRange(IEnumerable<T> entityes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddRangeAsync(IEnumerable<T> entityes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<T> entityes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRangeAsync(IEnumerable<T> entityes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Type ElementType => typeof(T);
+}
